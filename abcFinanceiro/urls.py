9ce6d2fc2ff234587dd42434848e7f00cc5f8bb1
Jose Paulo <neto.jpaulo@gmail.com>
2019-05-05 03:23:31 -0300

    Login

--- abcFinanceiro/urls.py
+++ abcFinanceiro/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from app_accounts import urls as app_accounts_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(app_accounts_urls)),
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_accounts/views.py
+++ app_accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import (HttpResponse,
+                        HttpResponseRedirect,
+                        HttpResponseForbidden,
+                        HttpResponseBadRequest,
+                        JsonResponse)
+from app_accounts.models import *
+
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class login(View):
+    context = {}
+    def get(self, request):
+        template_name = 'login.html'
+        print('entrou no get')
+        return render(resquest, template_name)
+    
+    def post(self, request):
+        context = {}
+        username = request.POST['username']
+        senha = request.POST['senha']
+        user = authenticate(user=username, password=senha)
+        if user is not None:
+            print('Está logando')
+            login(request, user)
+            if not request.user.is_anonymous:
+                usuario = DefaultUser.objects.get(usuario=request.user.id_usuario)
+                context['mensagem'] = 'Passou!'
+                return render(request, context)
+        else:
+            context['mensagem'] = 'Email ou senha incorretos!'
+            return render(request, 'login.html', context)
